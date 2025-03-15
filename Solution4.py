b75a6527f2e8624ea23ab6f2341e88967483f8aa
HariharanBuvi <hariharanramesh.cse@gmail.com>
2025-03-15 20:35:47 +0530

    Update Solution4.py

--- Solution4.py
+++ Solution4.py
@@ -1,3 +1,12 @@
-n=int(input())
-s=[int(i) for i in input().strip().split()]
-k=int(input())
+from itertools import combinations
+n=int(input());arr=[int(i) for i in input().strip().split()];k=int(input())
+lst=list(combinations(arr,k));mn=None
+for i in lst:
+    m=None
+    for j in range(1,len(i)):
+        x=abs(i[j]-i[j-1])
+        if m is None or x<m:
+            m=x
+    if mn is None or m>mn:
+        mn=m
+print(mn)
